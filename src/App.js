87f94c10f1c4a49ec507a9afe2421da2ad005393
Muhammad Sulaiman <sulaimanperson@gmail.com>
2021-08-21 15:03:08 +0500

    asd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Index from './Index.css';
+function App(){
+const [name, changeName] = useState('Muhammad Sulaiman')
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function ok(){
+  changeName('Muhammad Sulaiman The Best Programmer')
+}
+
+    return(
+      <div className="pa">
+         <div className="ch">
+         <span>{name}</span>
+           <button onClick={ok}>Click Me</button>
+         </div>
+       </div>
+      
+    )
 }
 
 export default App;
